8129724ac62da08bc5a32c8d2e8014b6282433e0
Daniel Ma <danielpluvia@outlook.com>
2019-03-22 19:05:32 +0800

    fixed the item width issue

--- DPCarouselCollectionViewFlowLayout/Classes/DPCarouselCvFlowLayout.swift
+++ DPCarouselCollectionViewFlowLayout/Classes/DPCarouselCvFlowLayout.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Xueqiang Ma on 18/3/19.
 //
-//  Inspired by https://github.com/ink-spot/UPCarouselFlowLayout.
+//  Inspired by https://github.com/ink-spot/UPCarouselFlowLayout
 //
 
 import UIKit
@@ -13,22 +13,30 @@ open class DPCarouselCvFlowLayout: UICollectionViewFlowLayout {
     fileprivate let sideItemAlpha: CGFloat = 0.4
     fileprivate let sideItemScale: CGFloat = 0.6
     
+    fileprivate let cellSize = CGSize(width: 300.0, height: 100.0)
+    
     open override func prepare() {
         super.prepare()
         guard let collectionView = collectionView else {return}
         collectionView.decelerationRate = .fast
         collectionView.isPagingEnabled = false
-        let xPadding = (collectionView.bounds.width - itemSize.width) / 2.0
-        let yPadding = (collectionView.bounds.height - itemSize.height) / 2.0
         
-        // set "sectionInset" and "minimumLineSpacing"
-        itemSize = CGSize(width: 300, height: 100)
-        if scrollDirection == .horizontal {
-            sectionInset = UIEdgeInsets(top: yPadding, left: xPadding, bottom: yPadding, right: xPadding)
-            minimumLineSpacing = -itemSize.width * 0.3
+        let availableBounds = collectionView.bounds.inset(by: collectionView.layoutMargins)
+        itemSize = CGSize(width: cellSize.width, height: cellSize.height)
+        
+        let xPadding = (availableBounds.width - itemSize.width - collectionView.contentInset.left - collectionView.contentInset.right) / 2.0
+        let yPadding = (availableBounds.height - itemSize.height - collectionView.contentInset.left - collectionView.contentInset.right) / 2.0
+        sectionInset = UIEdgeInsets(top: yPadding, left: xPadding, bottom: yPadding, right: xPadding)
+        if #available(iOS 11.0, *) {
+            sectionInsetReference = .fromSafeArea
         } else {
-            sectionInset = UIEdgeInsets(top: yPadding, left: xPadding, bottom: yPadding, right: xPadding)
-            minimumLineSpacing = -itemSize.height * 0.3
+            // Fallback on earlier versions
+        }
+        
+        if scrollDirection == .horizontal {
+            minimumLineSpacing = -itemSize.width * 0.4
+        } else {
+            minimumLineSpacing = -itemSize.height * 0.4
         }
     }
     
@@ -47,21 +55,22 @@ open class DPCarouselCvFlowLayout: UICollectionViewFlowLayout {
             let attributesList = self.layoutAttributesForElements(in: collectionView.bounds) else {
                 return super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
         }
+        let availableBounds = collectionView.bounds.inset(by: collectionView.layoutMargins)
         var closest: UICollectionViewLayoutAttributes!
         var offset: CGPoint!
         if scrollDirection == .horizontal {
-            let middle = collectionView.bounds.width / 2.0
+            let middle = availableBounds.width / 2.0
             let proposedCenter = proposedContentOffset.x + middle
-            let cellShiftOffset = collectionView.bounds.width / 2.0 - itemSize.width
+            let cellShiftOffset = availableBounds.width / 2.0 - itemSize.width
             closest = attributesList.sorted {
                 abs($0.center.x + cellShiftOffset - proposedCenter) < abs($1.center.x + cellShiftOffset - proposedCenter)
                 }.first ?? UICollectionViewLayoutAttributes()
             offset = CGPoint(x: closest.center.x + cellShiftOffset - middle,
                              y: proposedContentOffset.y)
         } else {
-            let middle = collectionView.bounds.height / 2.0
+            let middle = availableBounds.height / 2.0
             let proposedCenter = proposedContentOffset.y + middle
-            let cellShiftOffset = collectionView.bounds.height / 2.0 - itemSize.height
+            let cellShiftOffset = availableBounds.height / 2.0 - itemSize.height
             closest = attributesList.sorted {
                 abs($0.center.y + cellShiftOffset - proposedCenter) < abs($1.center.y + cellShiftOffset - proposedCenter)
                 }.first ?? UICollectionViewLayoutAttributes()
@@ -75,17 +84,18 @@ open class DPCarouselCvFlowLayout: UICollectionViewFlowLayout {
 extension DPCarouselCvFlowLayout {
     fileprivate func transform(attributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
         guard let collectionView = collectionView else {return attributes}
+        let availableBounds = collectionView.bounds.inset(by: collectionView.layoutMargins)
         var cvCenter: CGFloat = 0.0
         var normalizedCenter: CGFloat = 0.0
         var maxDistance: CGFloat = 0.0
         if scrollDirection == .horizontal {
-            cvCenter = collectionView.bounds.width / 2.0  // collection view center
+            cvCenter = availableBounds.width / 2.0  // collection view center
             normalizedCenter = attributes.center.x - collectionView.contentOffset.x // cell center
-            maxDistance = collectionView.bounds.width / 1.5
+            maxDistance = availableBounds.width / 1.5
         } else {
-            cvCenter = collectionView.bounds.height / 2.0  // collection view center
+            cvCenter = availableBounds.height / 2.0  // collection view center
             normalizedCenter = attributes.center.y - collectionView.contentOffset.y // cell center
-            maxDistance = collectionView.bounds.height / 1.5
+            maxDistance = availableBounds.height / 1.5
         }
         let distance = min(abs(cvCenter - normalizedCenter), maxDistance)
         let progress = (maxDistance - distance) / maxDistance   // cell to cv center's progress
@@ -95,9 +105,9 @@ extension DPCarouselCvFlowLayout {
         attributes.transform3D = CATransform3DScale(CATransform3DIdentity, scale, scale, 1)
         attributes.zIndex = Int(alpha * 1000)
         if scrollDirection == .horizontal {
-            attributes.center.x -= collectionView.bounds.width / 2.0 - itemSize.width
+            attributes.center.x -= availableBounds.width / 2.0 - itemSize.width
         } else {
-            attributes.center.y -= collectionView.bounds.height / 2.0 - itemSize.height
+            attributes.center.y -= availableBounds.height / 2.0 - itemSize.height
         }
         return attributes
     }
